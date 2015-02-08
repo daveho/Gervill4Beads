1060dde1121731c8385dd69974965db04910a315
David Hovemeyer <david.hovemeyer@gmail.com>
2015-02-08 09:14:02 -0500

    add copyright/license info to source files, a few javadoc tweaks

--- demo/io/github/daveho/gervill4beads/demo/Demo.java
+++ demo/io/github/daveho/gervill4beads/demo/Demo.java
@@ -1,3 +1,24 @@
+// Gervill4Beads - integrate Gervill with the Beads library
+// Copyright (c) 2015, David H. Hovemeyer <david.hovemeyer@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 package io.github.daveho.gervill4beads.demo;
 
 import io.github.daveho.gervill4beads.GervillUGen;
@@ -22,6 +43,8 @@ import net.beadsproject.beads.core.AudioContext;
  * <p>When you run the demo, you should be able to play notes
  * using the default instrument (piano) using Gervill's
  * default sound bank.
+ * 
+ * @author David Hovemeyer
  */
 public class Demo {
 	protected AudioContext ac;
