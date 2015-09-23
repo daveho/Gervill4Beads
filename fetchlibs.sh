0e282a6b846453b840b836c0374cdca2e64bcb5b
David Hovemeyer <david.hovemeyer@gmail.com>
2015-09-23 10:36:18 -0400

    whoops, we do need to download the Beads library, duh

--- fetchlibs.sh
+++ fetchlibs.sh
@@ -4,6 +4,12 @@
 # Since Gervill is built into JDK7 and JDK8, we don't need
 # to download it.  So, just download Beads.
 
+if [ ! -e Beads.zip ]; then
+	echo "Downloading Beads library..."
+	wget --progress=dot:mega http://www.beadsproject.net/downloads/Beads.zip
+	echo "done"
+fi
+
 jars='beads-io.jar beads.jar jarjar-1.0.jar jl1.0.1.jar jna.jar mp3spi1.9.4.jar org-jaudiolibs-audioservers-jack.jar org-jaudiolibs-audioservers-javasound.jar org-jaudiolibs-audioservers.jar org-jaudiolibs-jnajack.jar tools.jar tritonus_aos-0.3.6.jar tritonus_share.jar'
 
 mkdir -p lib
